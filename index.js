7df4d774c4f98cc887972cc428b89cf9dd0ee23d
David <david.owuor@student.moringaschool.com>
2022-12-10 11:33:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+
+
+
+function submitData(name, email){
+  const users ={
+    name : name,
+    email : email
+}
+  const config = {
+    method: 'POST',
+    headers: {
+      "Content-Type": 'application/json',
+      Accept: "application/json",
+    },
+    body: JSON.stringify(users)
+  };
+     return fetch('http://localhost:3000/users',config)
+    .then(res=>{
+      return res.json()
+    }).then(data=>{
+      document.querySelector('#id').textContent = data.id
+      document.querySelector('#name').textContent = data.name
+      document.querySelector('#email').textContent = data.email
+      //console.log(data)
+    }).catch(error=>{
+      let p = document.querySelector('#state')
+      p.textContent = error.message
+      //alert(error.message)
+    })
+}
+//submitData(users);
+document.querySelector('form').addEventListener('submit',(e)=>{
+  //const username = document.querySelector('#username')
+  submitData(e.target.name.value,e.target.email.value)
+})
+
+
